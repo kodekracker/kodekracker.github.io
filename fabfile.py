7b8c2341f1cb920ab43a91880b7747d7bb534db5
Akshay Pratap Singh <pratapakshay0@gmail.com>
2020-08-04 23:44:46 +0530

    upgrade code to python3.6

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,14 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
 import sys
 import json
 import settings
-from fabric.api import task, local
+from fabric import task
 from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader
 from pprint import pprint
+
 
 def gather_data(dir_name="."):
     """
@@ -18,30 +19,30 @@ def gather_data(dir_name="."):
     try:
         with open(dir_name+"/head.json") as head_json_data:
             head_data = json.load(head_json_data)
-            data.update({"head": head_data })
-            print "* Head Data Loaded..."
+            data.update({"head": head_data})
+            print("* Head Data Loaded...")
     except Exception as e:
-        print "Error while processing head.json"
+        print("Error while processing head.json")
         raise e
 
     # Read resource file and collect data
     try:
         with open(dir_name+"/resource.json") as resource_json_data:
             resource_data = json.load(resource_json_data)
-            data.update({"resource": resource_data })
-            print "* Resource Data Loaded"
+            data.update({"resource": resource_data})
+            print("* Resource Data Loaded")
     except Exception as e:
-        print "Error while processing resource.json"
+        print("Error while processing resource.json")
         raise e
 
     # Read body content file and collect data
     try:
         with open(dir_name+"/content.json") as content_json_data:
             content_data = json.load(content_json_data)
-            data.update({"content": content_data })
-            print "* Content Data Loaded..."
+            data.update({"content": content_data})
+            print("* Content Data Loaded...")
     except Exception as e:
-        print "Error while processing content.json"
+        print("Error while processing content.json")
         raise e
 
     # Get Google Analytics ID and add it to data
@@ -52,47 +53,54 @@ def gather_data(dir_name="."):
 
     return data
 
+
 @task
-def build(production=False):
+def build(c, production=False):
     """
     A task to build a project
     """
     env = Environment(loader=FileSystemLoader("templates"),
-        variable_start_string=settings.VARIABLE_START_STRING,
-        variable_end_string=settings.VARIABLE_END_STRING
-        )
+                      variable_start_string=settings.VARIABLE_START_STRING,
+                      variable_end_string=settings.VARIABLE_END_STRING
+                      )
     template = env.get_template("index.html")
     data = gather_data("app/data")
     with open("app/index.html", "w") as f:
         f.write(template.render(data=data, production=production))
-    print "\n ==> Project Build Successfully.\n"
+    print("\n ==> Project Build Successfully.\n")
+
 
 @task
-def live_run(port=8080):
+def live_run(c, port=8080):
     """
     A task which run a project as a livereload
     """
+    def build_site():
+        return build(c)
+
     # First build project for development
-    build()
+    build_site()
 
     # Set livereload server watchers and start it
     server = Server()
     server.watch('app/')
-    server.watch('app/data/*.json', build)
-    server.watch('templates/*.html', build)
+    server.watch('app/data/*.json', build_site)
+    server.watch('templates/*.html', build_site)
     server.serve(root="app/", open_url_delay=True, liveport=35729, port=port)
 
+
 @task
-def publish_github():
+def publish_github(c):
     """
     A task which publish a project to github pages
     """
     # Build project for production
-    build(production=True)
-    print "* Building project for production"
+    build(c, production=True)
+    print("* Building project for production")
 
     # Push to github master branch
-    local('ghp-import -r origin -b master -pm "(updated): site updated" app')
+    with c.prefix("source .venv/bin/activate"):
+        c.run('ghp-import -r origin -b master -pm "(updated): site updated" app', replace_env=False)
 
-    print '\n===> Successfully published to github pages.'
-    print "\n     Go to http://akshayon.net "
+    print('\n===> Successfully published to github pages.')
+    print("\n     Go to http://akshayon.net ")
